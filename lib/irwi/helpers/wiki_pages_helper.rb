d7f2db2410deedba0019f2cee4e62431ce2514b0
Evgeniy Zhukov <zhukov@paxnet.kr>
2020-04-18 07:01:30 +0700

    q

--- lib/irwi/helpers/wiki_pages_helper.rb
+++ lib/irwi/helpers/wiki_pages_helper.rb
@@ -36,7 +36,8 @@ module Irwi::Helpers::WikiPagesHelper
   end
 
   def wiki_content(text)
-    sanitize(auto_link(Irwi.config.formatter.format(wiki_linkify(wiki_show_attachments(text))).html_safe))
+    #sanitize(auto_link(Irwi.config.formatter.format(wiki_linkify(wiki_show_attachments(text))).html_safe))
+    auto_link(Irwi.config.formatter.format(wiki_linkify(wiki_show_attachments(text))))
   end
 
   def wiki_diff(old_text, new_text)
